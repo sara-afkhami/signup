817eaedf03bafe8d9b409fa3bb2628284d7d66ed
sirdanoosh <DJooybar@gmail.com>
2021-09-01 11:55:43 +0430

    fix: try to fix main again

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import './App.css'
+
+function App() {
+  const [stepNumber, setStepNumber] = useState(1);
+
+  const nextStageHandler = () => {
+    if(stepNumber<3){
+      setStepNumber((prev) => prev + 1);
+    }
+  };
+
+  const previousStageHandler = () =>{
+    if(stepNumber>1){
+      setStepNumber((prev) => prev - 1);
+    }
+  }
+
+  const formBodyCreator = (stage) => {
+    if (stage === 1) {
+      return <input name="userName" placeholder="userName" />;
+    } else if (stage === 2) {
+      return <input name="password" placeholder="password" />;
+    } else {
+      return <span>tamam shod</span>;
+    }
+  };
+
+  return (
+    <div className="login--background">
+      <div className="login--form">
+      <h1 className="font-Snaap text-purple-default">آکادمی نقطه</h1>
+            <h3>لطفا اطلاعات خود را وارد کنید</h3>
+        <form>{formBodyCreator(stepNumber)}</form>
+        <div className="btns">
+        <button className="btn1" type="button" onClick={() => nextStageHandler()}>
+          مرحله بعد
+        </button>
+        <button type="button" onClick={() => previousStageHandler()}>
+          مرحله قبل
+        </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
